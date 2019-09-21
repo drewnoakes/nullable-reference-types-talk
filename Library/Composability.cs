512a4673910dba4b7c0a49d46528243ea4086a41
Drew Noakes <git@drewnoakes.com>
2019-09-22 00:11:06 +1000

    WIP

---
+++ Library/Composability.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+
+class Composability
+{
+    void M()
+    {
+        List<string>   list1;
+        List<string>?  list2;
+        List<string?>  list3;
+        List<string?>? list4;
+
+        Func<string, string?> func;
+    }
+}
---
+++ Library/Warnings.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+
+class Warnings
+{
+    object nonInitializedField; // CS8618 non-nullable field not initialised
+
+    void AssignNullLiteral()
+    {
+        string s = null;        // CS8600 converting null literal to non-null type
+
+        object? o = null;
+        object o2 = o;          // CS8600 converting possible null value to non-null type
+    }
+
+    void ReceiveNull(string? s)
+    {
+        ReceiveNonNull(s);      // CS8604 possible 
+
+        s.ToUpper();            // CS8602 dereference of possibly null value
+
+        if (s != null)
+        {
+            ReceiveNonNull(s);
+
+            s.ToUpper();
+        }
+    }
+
+    void ReceiveNonNull(string s)
+    {
+    }
+}
