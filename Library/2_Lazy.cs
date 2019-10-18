48f27c0d2714491fa8cfd2e7c1ff8667dbcc93b0
Drew Noakes <git@drewnoakes.com>
2019-10-19 06:41:39 +1100

    Change lazy example

---
+++ Library/2_Lazy.cs
@@ -0,0 +1,22 @@
+﻿class Lazy
+{
+    private string _value;
+
+    public string Value
+    {
+        get
+        {
+            if (_value == null)
+            {
+                _value = Factory();
+            }
+
+            return _value;
+        }
+    }
+
+    private static string Factory()
+    {
+        return "Expensive value";
+    }
+}
